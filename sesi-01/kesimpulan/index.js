1711cc9239f60f000ae3b706adf33f8079c905b3
ahmadsaipullah <ahmadsaipullah911@gmail.com>
2022-08-25 17:48:25 +0700

    mengedit kesimpulan

--- sesi-01/kesimpulan/index.js
+++ sesi-01/kesimpulan/index.js
@@ -1,8 +1,55 @@
 //#1
 
-apaItuVariabel = "variabel adalah ";
+apaItuVariabel = "variabel adalah tempat untuk menampung sebuah nilai ";
 console.log(apaItuVariabel);
 
 //#2
-pentingVariabel = "pentingnya variabel...";
+pentingVariabel = "pentingnya variabel merupakan hal yang sangat penting dalam sebuah penelitian, karena sangat tidak memungkinkan bagi seorang peneliti melakukan penelitian tanpa variabel.";
 console.log(personalbar);
+
+//#3
+mendklarasikanVariabel = "dimaksudkan untuk memberitahu kompiler (compiler) Java tentang nama variabel dan tipe data yang diwakili.";
+console.log(mendklarasikanVariabel);
+
+//#4
+mengisiVariabelDenganNilaiAtauValue = "mengisi variabel dengan nilai dan menghasilkan value";
+console.log(mengisiVariabelDenganNilaiAtauValue);
+
+//#5
+memberiNamaVariabelYangBaik = "Nama variabel wajib diawali dengan garis bawah atau underscore _ atau huruf, tidak boleh angka, lalu karakter berikutnya bisa angka, huruf, dan garis bawah.";
+console.log(memberiNamaVariabelYangBaik);
+
+//#6
+apaItuTypeData = "Tipe data adalah klasifikasi variable untuk menentukan data yang akan disimpan ke dalam memori.";
+console.log(apaItuTypeData);
+
+//#7
+berbagaiJenisTipeData = "integer, string, floating point, char, boolean, array";
+console.log(berbagaiJenisTipeData);
+
+//#8
+tipeDataNumberDanPengoperasiannya = "Number : data dalam bentuk angka dengan pilihan field size yang terdiri dariByte, Integer, Long Integer, Single, Double, General Number, Currency, Standard, Percent, Scientific";
+console.log(tipeDataNumberDanPengoperasiannya);
+
+//#9
+tipeDataStringDanManipulasinya = "String adalah sebuah array yang bertipe char yang diakhiri dengan karakter null contoh, deklarasi dibawah ini merupakan deklasai sebuah array yang bertipe char, dan bisa disamakan dengan deklarasi sebuah string dengan nama st";
+console.log(tipeDataStringDanManipulasinya);
+
+//#10
+tipeDataBoolean = "true dan false";
+
+//#11
+bagaimanaMengetahuiTipeDataDariSebuahVariabel = "Dengan memanggil fungsi type(nama_variabel) , kita akan bisa mengetahui tipe data dari sebuah variabel.";
+console.log(bagaimanaMengetahuiTipeDataDariSebuahVariabel);
+
+//#12
+apaItuArray = "Array adalah larik yang berisi kumpulan data dengan tipe serupa. ";
+console.log(apaItuArray);
+
+//#13
+apaItuJenisArray = "Apa itu tipe data array dan contohnya? merupakan salah satu tipe data terstruktur (structured data type) yang terdiri dari sejumlah komponen-komponen yang mempunyai tipe data yang sama. ";
+console.log(apaItuJenisArray);
+
+//#14
+mengisiArray = "huruf[2] = 'z';";
+console.log(mengisiArray);
